645b939af8ff2ed70a199778dd678bf6da438554
Lautaro Gando <lautarogandodev@gmail.com>
2024-11-06 17:46:08 -0300

    Prueba

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Banner: React.FC = (): React.ReactElement => {
+
+    return (
+
+        <h1>Soy el Banner</h1>
+
+    );
+
+};
+
+export default Banner;
