cb301c1b7b48cc9b16b25888f03f84626fcd4d54
Lautaro Gando <lautarogandodev@gmail.com>
2024-11-09 18:16:13 -0300

    Estructuración

---
+++ src/app/language/[slug]/[courseDetail]/CourseDetail.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Course: React.FC = (): React.ReactElement => {
+
+    return (
+
+        <div>
+            
+        </div>
+
+    );
+
+};
+
+export default Course;
