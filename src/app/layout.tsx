b4efc72ddc3164c66998b726665cc97a096e03dd
Lautaro Gando <lautarogandodev@gmail.com>
2024-11-11 19:09:30 -0300

    Agregando logo a la web.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  icons: '/assets/icons/logo.png',
   title: "Rompiendo Barreras",
   description: "Plataforma de cursos de idiomas.",
 };
