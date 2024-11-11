2eef7cd6f8aa29973dd0390762ce721e0f486d8a
Agustin Ancona <anconaa54@gmail.com>
2024-11-11 19:19:02 -0300

    Resolviendo Conflictos (1)

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      //? LENGUAGES
-      "www.advantagelearningcenter.com",
-      "encrypted-tbn0.gstatic.com",
-      "upload.wikimedia.org",
-      "ecdn.teacherspayteachers.com",
-      "ih1.redbubble.net",
-      "thumbs.dreamstime.com",
-      "www.shutterstock.com",
-
-      //? CURSOS
       "cdn01.alison-static.net",
       "thinkinginenglish.blog",
       "onlineitalianclasses.com",
@@ -20,6 +10,13 @@ const nextConfig: NextConfig = {
       "learn-portuguese-now.com",
       "bcci.edu.au",
       "www.bcci.edu.au",
+      "www.advantagelearningcenter.com",
+      "encrypted-tbn0.gstatic.com",
+      "upload.wikimedia.org",
+      "ecdn.teacherspayteachers.com",
+      "ih1.redbubble.net",
+      "thumbs.dreamstime.com",
+      "www.shutterstock.com",
     ],
   },
 };
