017d81c1c25779dc64c6981611d55bdd0d994612
patoveras7 <patricioverasc@gmail.com>
2024-11-11 19:11:36 -0300

    commit for Demo 1

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,17 @@ const nextConfig: NextConfig = {
       'cdn01.alison-static.net',
       'thinkinginenglish.blog',
       'onlineitalianclasses.com',
-      'encrypted-tbn0.gstatic.com',
       'cursa-json-routes.s3.amazonaws.com',
       'learn-portuguese-now.com',
       'bcci.edu.au',
-      'www.bcci.edu.au' // Agregado el subdominio www.bcci.edu.au
+      'www.bcci.edu.au',
+      "www.advantagelearningcenter.com",
+      "encrypted-tbn0.gstatic.com",
+      "upload.wikimedia.org",
+      "ecdn.teacherspayteachers.com",
+      "ih1.redbubble.net",
+      "thumbs.dreamstime.com",
+      "www.shutterstock.com"
     ],
   },
   // Otras configuraciones opcionales aquí
