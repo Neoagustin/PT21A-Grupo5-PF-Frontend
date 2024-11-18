bf5797459e8e0f4b0c96daedb68029e62e567d77
Agustin Ancona <anconaa54@gmail.com>
2024-11-18 16:55:37 -0300

    Ignorando Eslint en vercel

--- src/app/checkout/[slug]/Checkout.tsx
+++ src/app/checkout/[slug]/Checkout.tsx
@@ -1,16 +1,12 @@
 import CheckoutView from "@/components/CheckoutComponents/checkoutView/CheckoutView";
 import React from "react";
 
-export const Checkout: React.FC<{ params: { slug: string } }> = ({ params }: { params: { slug: string } }): React.ReactElement => {
-
-    return (
-
-    
-        <CheckoutView slug={params.slug} />
-        
-
-    );
-
+export const Checkout: React.FC<{ params: { slug: string } }> = ({
+  params,
+}: {
+  params: { slug: string };
+}): React.ReactElement => {
+  return <CheckoutView slug={params.slug} />;
 };
 
 export default Checkout;
